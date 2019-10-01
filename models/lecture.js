89980644abf60c647241dad544b49782cfc68f44
ThaTruth24 <thankgodegbo@gmail.com>
2019-10-01 19:41:04 +0100

    changed the nested materails schema and made the quiz and answers schema nested in the materials schema

--- models/lecture.js
+++ models/lecture.js
@@ -1,10 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const QuizSchema = new Schema({
+  question: String,
+  correct_answer: String,
+  materialid: String
+});
+
+const AnswersSchema = new Schema({
+  a: String,
+  b: String,
+  c: String,
+  d: String,
+  e: String
+})
+
 const MaterialsSchema = new Schema({
+  title: String,
+  colour: String,
+  filetype: String,
   material: String,
-  name: String,
-  topicid: String
+  topicid: String,
+  quiz: [QuizSchema],
+  answers: [AnswersSchema]
 });
 
 const SubTopics = new Schema({
