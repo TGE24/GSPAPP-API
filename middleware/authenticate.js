4480689db0ffcb43c5995d342253c25c2ea2bc31
ThaTruth24 <thankgodegbo@gmail.com>
2019-10-03 19:22:53 +0100

    Made Changes

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -9,13 +9,13 @@ const authenticate = async (req, res, next) => {
     //     throw new Error('Request cookie is invalid.');
     // }
     // query the database using the session token and checking for status of valid
-    const session = await Session.findOne({ token, status: "valid" });
-    if (!session) {
-      // if the session is not found with the provided token, clear the cookie
-      // e.g. user session expired on server, but the browser still has the cookie
-      res.clearCookie("token");
-      throw new Error("Your session has expired. You need to log in.");
-    }
+    // const session = await Session.findOne({ token, status: "valid" });
+    // if (!session) {
+    //   // if the session is not found with the provided token, clear the cookie
+    //   // e.g. user session expired on server, but the browser still has the cookie
+    //   res.clearCookie("token");
+    //   throw new Error("Your session has expired. You need to log in.");
+    // }
     // set the session retrieved from db on the request object
     // the request object will be passed on to the next() call
     req.session = session;
