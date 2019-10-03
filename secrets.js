b696108b33b580bf5a7c950afba1dc54d4e65eac
ThankGod Egbo <34455592+TGE24@users.noreply.github.com>
2019-10-03 15:34:08 +0100

    Changed the database to the remote one

--- secrets.js
+++ secrets.js
@@ -1,5 +1,5 @@
 const secrets = {
-  dbUri: process.env.DB_URI || "mongodb://localhost/gspdb"
+  dbUri: process.env.DB_URI || "mongodb://tg:1some2thing@ds217678.mlab.com:17678/gsp-api"
 };
 
 //remote datatbase mongodb://tg:1some2thing@ds217678.mlab.com:17678/gsp-api
